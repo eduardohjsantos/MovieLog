295cfa195e91f5594c3dc1c55cdfc4d38e55b5a7
eduardohjsantos <84049370+eduardohjsantos@users.noreply.github.com>
2026-05-12 16:55:15 -0300

    Initial commit

---
+++ src/test/java/dev/movielog/MovielogApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.movielog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovielogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
